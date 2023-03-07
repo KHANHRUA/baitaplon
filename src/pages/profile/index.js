07370d119c95914d543b6dbd65d433030fbdf4e9
KHANHRUA <tranbk1@gmail.com>
2023-03-07 16:30:47 +0700

    sapcomodal

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
 import styles from "./styles.module.css";
 import khanh from "./khanh.json";
-import cong from "./cong.json";
-import vinh from "./vinh.json";
-import hoang from "./hoang.json";
-import thang from "./pthang.json";
-import Modal from 'react-modal';
 
 function Profile() {
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
   const [Profile, setProfile] = useState(khanh);
+  const handleopen = () => {
+    setIsOpen(true);
+  };
   const callpost = (temp) => {
-    console.log("aaaa");
     return (
       <div className={styles.post}>
         <div className={styles.nameandava}>
@@ -29,32 +26,24 @@ function Profile() {
     }
     return a;
   };
-  const handleClick = () => {
-    setProfile(khanh);
-  };
-  const handleClick1 = () => {
-    setProfile(cong);
-  };
-  const handleClick2 = () => {
-    setProfile(vinh);
-  };
-  const handleClick3 = () => {
-    setProfile(hoang);
-  };
-  const handleClick4 = () => {
-    setProfile(thang);
-  };
   return (
     <div className={styles.container}>
+      {modalIsOpen && (
+        <div>
+          <div></div>
+        </div>
+      )}
       <div>
         <div className={styles.content}>
           <h1 className={styles.persionalinformation}>personal information</h1>
-          <div className={styles.infor}>Date of birth: {Profile.dateofbirth}</div>
+          <div className={styles.infor}>
+            Date of birth: {Profile.dateofbirth}
+          </div>
           <div className={styles.infor}>Hometown: {Profile.hometown}</div>
           <div className={styles.infor}>Study at: {Profile.school}</div>
           <div className={styles.infor}>Branch: {Profile.branch}</div>
           <div className={styles.containbuttonupdateinfor}>
-            <button className={styles.buttonupdateinfor} onClick={setIsOpen(true)}>
+            <button className={styles.buttonupdateinfor} onClick={handleopen}>
               update information
             </button>
           </div>
@@ -68,25 +57,6 @@ function Profile() {
         </div>
         <div className={styles.name}>
           <div>{Profile.name}</div>
-        </div>
-
-
-        <div className={styles.link}>
-          <button className={styles.button} onClick={handleClick}>
-            khánh
-          </button>
-          <button className={styles.button} onClick={handleClick1}>
-            Công
-          </button>
-          <button className={styles.button} onClick={handleClick2}>
-            Vinh
-          </button>
-          <button className={styles.button} onClick={handleClick3}>
-            Hoàng
-          </button>
-          <button className={styles.button} onClick={handleClick4}>
-            Thắng
-          </button>
         </div>
         <div className={styles.allpost}>{allpost()}</div>
       </div>
--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Log from "../pages/log";
 import Layout from "../layouts";
 import Profile from "../pages/profile";
-import Home  from "../pages/home";
+import Home from "../pages/home";
 
 function AppRouter() {
   return (
@@ -11,8 +11,9 @@ function AppRouter() {
       <Routes>
         <Route path="/" element={<Log />} />
         <Route path="/home" element={<Layout />}>
-          <Route path="/home" element={<Home/>} />
+          <Route path="/home" element={<Home />} />
           <Route path="/home/profile" element={<Profile />} />
+          <Route path="/home/profile/khanh" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
